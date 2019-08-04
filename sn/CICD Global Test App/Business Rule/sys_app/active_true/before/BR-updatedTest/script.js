b3317215fbf20dbdcc3613f5223f6be693f85a4e
System Administrator <admin@example.com>
2019-08-03 23:47:08 -0700

    Changes by 'System Administrator' on 'CICD Global Test App – 1.0.0' -- Build #1

---
+++ sn/CICD Global Test App/Business Rule/sys_app/active_true/before/BR-updatedTest/script.js
@@ -0,0 +1,14 @@
+/* 
+ * Application : CICD Global Test App
+ * ClassName   : sys_script
+ * Created On  : 2019-07-18 17:23:12
+ * Created By  : admin
+ * Updated On  : 2019-07-18 17:23:37
+ * Updated By  : admin
+ * URL         : /sys_script.do?sys_id=959e4b94db323300545aeb41ca96196a
+ */
+(function executeRule(current, previous /*null when async*/) {
+
+	// Add your code here
+
+})(current, previous);
---
+++ sn/CICD Global Test App/Script Include/active_true/x_373731_cicd_glob.sampleScript.js
@@ -0,0 +1,29 @@
+/* 
+ * Application : CICD Global Test App
+ * ClassName   : sys_script_include
+ * Created On  : 2019-08-04 06:45:47
+ * Created By  : admin
+ * Updated On  : 2019-08-04 06:46:00
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=c2202471dbcb7300545aeb41ca9619a6
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_373731_cicd_glob.module:sys_script_include
+ */
+var sampleScript = Class.create();
+sampleScript.prototype = /** @lends x_373731_cicd_glob.module:sys_script_include.sampleScript.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+		return true;
+    },
+
+    type: 'sampleScript'
+};
